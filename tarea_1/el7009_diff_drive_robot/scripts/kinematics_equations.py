840e084308a4841249fa2a322e5d3a73e5d689e3
tnacho-23 <ignacio.romero.a@ug.uchile.cl>
2025-03-27 14:21:00 -0300

    rotation and forward inverse kinematics functions

--- tarea_1/el7009_diff_drive_robot/scripts/kinematics_equations.py
+++ tarea_1/el7009_diff_drive_robot/scripts/kinematics_equations.py
@@ -16,7 +16,7 @@ class Kinematics(Node):
         self.publisher_right_ = self.create_publisher(Float64, 'right_wheel_cmd_vel', 10)
         self._loop_rate = self.create_rate(10.0, self.get_clock())
 
-def rotate(theta: float):
+def rotate(theta: float, dt: float = 5.0):
     rclpy.init(args=None)
     l = 0.35
     r = 0.05
@@ -51,5 +51,41 @@ def rotate(theta: float):
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
+def forward(x: float, dt: float = 5.0):
+    rclpy.init(args=None)
+    l = 0.35
+    r = 0.05
+    v = x/dt
+
+    w = v/r
+
+    # Spin in a separate thread so that the clock is updated
+    minimal_publisher = Kinematics()
+    thread = threading.Thread(target=rclpy.spin, args=(minimal_publisher,))
+    thread.start()
+
+    # Publish speeds for dt seconds
+    minimal_publisher.get_logger().info("Publishing speeds for 4 seconds")
+    for i in range(dt*10):
+        msg = Float64()
+        msg.data = w
+        minimal_publisher.publisher_left_.publish(msg)
+        msg.data = w
+        minimal_publisher.publisher_right_.publish(msg)
+        minimal_publisher.get_logger().info("publishing speeds")
+        minimal_publisher._loop_rate.sleep()
+
+    # Stop the robot
+    minimal_publisher.get_logger().info("Stopping the robot")
+    msg = Float64()
+    msg.data = 0.0
+    minimal_publisher.publisher_left_.publish(msg)
+    minimal_publisher.publisher_right_.publish(msg)
+
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
+
 if __name__ == '__main__':
-    rotate(3.14/2)
+    rotate(3.14/2, 5)
+    forward(5, 10)
+    rotate(-3.14/2, 5)
