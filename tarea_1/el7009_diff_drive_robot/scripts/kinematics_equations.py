64f6c4b30b94452292897b61d40ea1655de03f24
tnacho-23 <ignacio.romero.a@ug.uchile.cl>
2025-03-27 21:02:36 -0300

    go to position function

--- tarea_1/el7009_diff_drive_robot/scripts/kinematics_equations.py
+++ tarea_1/el7009_diff_drive_robot/scripts/kinematics_equations.py
@@ -6,23 +6,23 @@ import rosidl_parser
 import threading
 from std_msgs import msg
 from std_msgs.msg import   Float64
+import numpy as np
 
+l = 0.3076
+r = 0.045
 
 class Kinematics(Node):
 
     def __init__(self):
-        super().__init__('example_controller')
+        super().__init__('Kinematics')
         self.publisher_left_ = self.create_publisher(Float64, 'left_wheel_cmd_vel', 10)
         self.publisher_right_ = self.create_publisher(Float64, 'right_wheel_cmd_vel', 10)
         self._loop_rate = self.create_rate(10.0, self.get_clock())
 
-def rotate(theta: float, dt: float = 5.0):
+def rotate(theta: float, dt: int = 10):
     rclpy.init(args=None)
-    l = 0.35
-    r = 0.05
     dt = 5
     v = (theta*l)/(2*dt)
-
     w = v/r
 
     # Spin in a separate thread so that the clock is updated
@@ -51,10 +51,8 @@ def rotate(theta: float, dt: float = 5.0):
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
-def forward(x: float, dt: float = 5.0):
+def forward(x: float, dt: int = 15):
     rclpy.init(args=None)
-    l = 0.35
-    r = 0.05
     v = x/dt
 
     w = v/r
@@ -85,13 +83,28 @@ def forward(x: float, dt: float = 5.0):
     minimal_publisher.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
-    rotate(2.8/2, 10)
-    forward(3.6, 20)
-    rotate(2.8/2, 10)
-    forward(3.0, 20)
-    rotate(2.8/2, 10)
-    forward(3.6, 20)
-    rotate(2.8/2, 10)
-    forward(3.5, 20)
+def move(x: float, y: float, theta: float, dtx: int= 15, dtt: int = 10):
+    if x == 0 and y == 0:
+        target_angle = 0
+    else:
+        target_angle = np.arctan2(y, x)
+    
+    rotate(target_angle, dtt)
+    
+    distance = np.sqrt(x**2 + y**2)
+    
+    forward(distance, dtx)
+    
+    final_rotation = theta - target_angle
+    rotate(final_rotation, dtt)
 
+if __name__ == '__main__':
+    # rotate(3/2, 10)
+    # forward(3.5, 20)
+    # rotate(3/2, 10)
+    # forward(3.0, 20)
+    # rotate(3/2, 10)
+    # forward(3.5, 20)
+    # rotate(3/2, 10)
+    # forward(3.5, 20)
+    move(3, -2, 3.14)
